fa7a3c457c7097af9880b837032d1d43b8e5cba5
ardynn14 <ardynn.d@gmail.com>
2019-08-15 20:04:20 +0700

    add login , register

--- server/routes/indexRoutes.js
+++ server/routes/indexRoutes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router()
-const GifRouter = require('./gifRoutes')
-// const UserRouter = require('./userRouter')
+// const GifRouter = require('./gifRoutes')
+const UserRouter = require('./userRoutes')
 
-router.use('/gif', GifRouter)
-// router.use('/users', UserRouter)
+router.use('/users', UserRouter)
+// router.use('/gif', GifRouter)
 
 module.exports = router;
